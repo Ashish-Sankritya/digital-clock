6d97888fa2fc3c4edf9f4c060abb7282a5b3420b
Ashish-Sankritya <ashishsankritya@gmail.com>
2022-03-21 12:36:30 +0530

    first commit

---
+++ clock.js
@@ -0,0 +1,14 @@
+const clock = () => {
+  let hours = document.getElementById("hours");
+  let minutes = document.getElementById("minutes");
+  let seconds = document.getElementById("seconds");
+
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+};
+setInterval(clock, 1000);
